657e384415144958222d97bf47b777c2b3a510ed
Emilly <emymonteiro.sweetie@gmail.com>
2021-12-25 20:05:30 -0400

    Starting Project

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+    <div id="container">
+      <div id="Wrapper">
+          A
+      </div>
+    </div>
+</template>
+
+<style>
+    #Wrapper{
+        height:100%;
+        max-width: 1280px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+    colors:{
+      primary: '#000',
+      secondary: '#15181C',
+      search: '#202327',
+      white: '#D9D9D9',
+      twhite: '#fff',
+      gray: '#7A7A7A',
+      outline: '#2F3336',
+      like: '#E8265E',
+      twitter: '#33A1F2',
+      tdarkh: '#011017',
+      tlighth: '#2C8ED6'
+    }
+  },
+  plugins: [],
+}
